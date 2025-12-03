c119ec348fbbe9ec6ef8dd513129f97a6899dd1e
yazumichan <144269263+yazumichan@users.noreply.github.com>
2025-12-03 20:24:19 +0700

    Remove GET function and export dynamic config

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -1,19 +1,2 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({
-    type: "frame",
-    version: "vNext",
-    image: "https://chibipurple-b9wq.vercel.app/chibi.png",
-    buttons: [
-      {
-        label: "Mint 0.0001 BASE",
-        action: "mint",
-        mint_address: "0x8A3De524F153282D66150c46CD5191D24a6aEdC1",
-        mint_price: "0.0001"
-      }
-    ]
-  });
-}
-
-export const dynamic = "force-dynamic";
+const nextConfig = {};
+module.exports = nextConfig;
