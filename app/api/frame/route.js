27a4ac02230dbde4e0ca9ecec06702c6fe14ad94
yazumichan <144269263+yazumichan@users.noreply.github.com>
2025-12-03 22:10:01 +0700

    Update route.js

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -1,10 +1,14 @@
 export async function GET() {
   return Response.json({
-    type: "frame",
-    version: "vNext",
-    image: "https://chibipurple-b9wq.vercel.app/chibi1.png",
-    buttons: [
-      { label: "Next", action: "post" }
-    ]
+    frame: {
+      version: "vNext",
+      image: "https://chibipurple-b9wq.vercel.app/chibi1.png",
+      buttons: [
+        { label: "Open", action: "post" },
+        { label: "Mint", action: "post" }
+      ],
+      post_url: "https://chibipurple-b9wq.vercel.app/api/frame"
+    }
   });
 }
+
