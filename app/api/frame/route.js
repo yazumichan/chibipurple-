cada1c0b415323ca128a8a34aa3338aafadb2a61
yazumichan <144269263+yazumichan@users.noreply.github.com>
2025-12-03 20:38:58 +0700

    Refactor GET function to return updated frame data

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -1,13 +1,10 @@
-{
-  "type": "frame",
-  "version": "vNext",
-  "image": "https://chibipurple-b9wq.vercel.app/chibi.png",
-  "buttons": [
-    {
-      "label": "Mint 0.0001 BASE",
-      "action": "mint",
-      "mint_address": "0x8A3De524F153282D66150c46C5D191D24a6aEdC1",
-      "mint_price": "0.0001"
-    }
-  ]
+export async function GET() {
+  return Response.json({
+    type: "frame",
+    version: "vNext",
+    image: "https://chibipurple-b9wq.vercel.app/chibi1.png",
+    buttons: [
+      { label: "Next", action: "post" }
+    ]
+  });
 }
