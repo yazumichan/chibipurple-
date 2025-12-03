1bc62383f80b8b49bbaf5d41046aadf13c67d066
yazumichan <144269263+yazumichan@users.noreply.github.com>
2025-12-03 20:27:05 +0700

    Update route.js to include frame configuration

--- app/api/frame/route.js
+++ app/api/frame/route.js
@@ -1,2 +1,13 @@
-const nextConfig = {};
-module.exports = nextConfig;
+{
+  "type": "frame",
+  "version": "vNext",
+  "image": "https://chibipurple-b9wq.vercel.app/chibi.png",
+  "buttons": [
+    {
+      "label": "Mint 0.0001 BASE",
+      "action": "mint",
+      "mint_address": "0x8A3De524F153282D66150c46C5D191D24a6aEdC1",
+      "mint_price": "0.0001"
+    }
+  ]
+}
